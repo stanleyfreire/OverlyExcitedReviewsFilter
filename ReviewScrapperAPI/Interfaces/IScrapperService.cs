78b9aa946cfbe5ab6d5921d6e2b0e18686fb4d83
Stanley Freire <stanley_freire@hotmail.com>
2021-11-19 00:53:29 -0300

    First Version

---
+++ ReviewScrapperAPI/Interfaces/IScrapperService.cs
@@ -0,0 +1,13 @@
+﻿using ReviewScrapperAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewScrapperAPI.Interfaces
+{
+    public interface IScrapperService
+    {
+        public Task<List<Review>> FetchReviews(int numberOfPages);
+    }
+}
